36ab4f1db9a410d4155f8f57f3abecc620b08737
Daniel Craigmile <daniel.craigmile@mutualmobile.com>
2013-04-28 01:23:09 -0500

    cleaned up

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ setup(name='air',
       packages=['rair'],
       author='Daniel Craigmile',
       author_email='daniel.craigmile@mutualmobile.com',
-      install_requires=['requests==0.14.2', 'configobj', 'jira_python', 'sh',
-            'unittest2'],
+      install_requires=[
+          'requests==0.14.2',
+          'configobj',
+          'jira_python',
+          'sh',
+          'unittest2',
+      ],
       scripts=['air']
       )
